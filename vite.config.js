9e1f6e5d1acb83ca43b5ff9eb4c63ba63b85a524
Ced <cedric.rollo@gmail.com>
2023-03-18 22:17:12 +0100

    🚀 deploy script

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/foosball-chrono/',
+  base: 'foosball-chrono',
   plugins: [react()],
 })
